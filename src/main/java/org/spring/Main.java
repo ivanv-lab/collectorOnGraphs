cf91b93c15c4fa798de2db76fe3ba00b79e9f0af
ivanv-lab <ivanvozisov33@yandex.ru>
2025-03-18 11:16:11 +0300

    Реализация графа

--- src/main/java/org/spring/Main.java
+++ src/main/java/org/spring/Main.java
@@ -3,9 +3,18 @@ package org.spring;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
 
+        List<Task> graph=new ArrayList<>();
+        graph=new Task().createGraph(15);
+
+        for(Task task:graph){
+            task.getLog();
+        }
     }
 }
--- src/main/java/org/spring/Task.java
+++ src/main/java/org/spring/Task.java
@@ -1,16 +1,30 @@
 package org.spring;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class Task {
     int n;
     Task next;
     Task prev;
+    String log;
 
-    public Task(Task next, Task prev) {
-        this.n++;
+    public Task(){}
+
+    public Task(int value,Task next, Task prev) {
+        this.n=value;
         this.next = next;
         this.prev = prev;
+        this.log="Выполнена задача сборки номер "+n;
+    }
+
+    public String getLog() {
+        return log;
+    }
+
+    public void setLog(String log) {
+        this.log = log;
     }
 
     public int getN() {
@@ -35,5 +49,17 @@ public class Task {
 
     public void setPrev(Task prev) {
         this.prev = prev;
+    }
+
+    public List<Task> createGraph(int taskCount){
+        Random random=new Random();
+        List<Task> graph=new ArrayList<>();
+        Task nullTask=new Task(random.nextInt(),null,null);
+        graph.add(nullTask);
+        for(int i=0;i<taskCount;i++){
+            graph.add(new Task(random.nextInt(),graph.get(i-1).next,null));
+        }
+
+        return graph;
     }
 }
